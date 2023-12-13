16d8b08c34db23b63b9aaef1ff1572e5e2ac0924
LindaAmnell <amnell3@hotmail.com>
2023-12-13 15:27:40 +0100

    fixat små saker och söksida samt team sidan

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 import { startbtn, pokemonList } from "./startScreen.js";
-import { teamButton } from "./teamScreen.js";
-
+import { teamButton, pokemonTeamContainer } from "./teamScreen.js";
 const searchInput = document.querySelector("#search-pokémon");
 const pokemonContainer = document.querySelector("#pokèmon-list");
 const pokemonListaContainer = document.querySelector("#pokèmon ");
+const teamCount = document.querySelector("#team-count");
+const reservCount = document.querySelector("#reserve-team-count");
+const neadCountTeam = document.querySelector("#team-mates-count");
+const teamMateContainer = document.querySelector(".team-mates");
+const teamContainer = document.querySelector(".pokemon-team");
+const reservContainer = document.querySelector(".reserve-team");
+let countTeam = 0;
+let countReserv = 0;
+let countNeadTeam = 3;
 let teamList = [];
 let reservList = [];
 searchInput.addEventListener("keyup", async () => {
@@ -18,17 +26,16 @@ searchInput.addEventListener("keyup", async () => {
     const element = document.createElement("li");
     const button = document.createElement("button");
     element.classList.add("element");
+    element.innerText = pokemon.name.toUpperCase();
 
     if (pokemon.bild) {
       const bild = document.createElement("img");
       bild.src = `${pokemon.bild}`;
-      element.innerText = `${pokemon.name}`.toUpperCase();
       element.appendChild(bild);
       bild.classList.add("bild");
     } else {
       const bild2 = document.createElement("img");
       bild2.src = "./bilder/anonymbild.jpg";
-      element.innerText = `${pokemon.name}`.toUpperCase();
       element.appendChild(bild2);
       bild2.classList.add("bild2");
     }
@@ -44,30 +51,69 @@ searchInput.addEventListener("keyup", async () => {
       const button = document.createElement("button");
       const label = document.createElement("label");
       button.innerText = "spara";
-      label.innerText = "Smecknamn";
+      label.innerText = "Smeknamn";
       inputNickName.value = `${pokemon.name}`;
       nickName.appendChild(label);
       nickName.appendChild(inputNickName);
       nickName.appendChild(button);
       pokemonContainer.append(nickName);
       nickName.classList.add("nick-name");
-
-      button.addEventListener("click", () => {
-        addToTeamList({ name: inputNickName.value, bild: pokemon.bild });
+      const someIndex = button.addEventListener("click", () => {
+        addToTeamList(
+          { name: inputNickName.value, bild: pokemon.bild },
+          someIndex
+        );
         nickName.classList.remove("nick-name");
         nickName.classList.add("nick-name-hide");
       });
     });
   });
 });
-function addToTeamList(pokemon) {
+function addToTeamList(pokemon, index) {
   if (teamList.length < 3) {
     teamList.push(pokemon);
     console.log(teamList);
+    countTeam++;
+    countNeadTeam--;
+    teamCount.innerText = countTeam;
+    neadCountTeam.innerHTML = countNeadTeam;
+    teamContainer.classList.add("show");
+    if (teamList.length == 3) {
+      teamMateContainer.classList.add("hide");
+    }
   } else {
     reservList.push(pokemon);
     console.log(reservList);
+    countReserv++;
+    reservCount.innerText = countReserv;
+    reservContainer.classList.add("show");
   }
 }
-
-export { pokemonContainer, pokemonListaContainer, teamList, reservList };
+function deletePokemon(event) {
+  const index = event.currentTarget.dataset.index;
+  teamList.splice(index, 1);
+  //   console.log(teamList);
+  //   countTeam--;
+  //   countNeadTeam++;
+  countReserv++;
+  //   teamCount.innerText = countTeam;
+  //   neadCountTeam.innerHTML = countNeadTeam;
+  reservCount.innerText = countReserv;
+  teamMateContainer.classList.remove("hide");
+}
+function deletePokemonReserv(event) {
+  const index = event.currentTarget.dataset.index;
+  reservList.splice(index, 1);
+  //   console.log(teamList);
+  countReserv--;
+  reservCount.innerText = countReserv;
+}
+export {
+  pokemonContainer,
+  pokemonListaContainer,
+  teamList,
+  reservList,
+  countTeam,
+  deletePokemon,
+  deletePokemonReserv,
+};
--- teamScreen.js
+++ teamScreen.js
@@ -1,5 +1,10 @@
-import { searchScreen } from "./startScreen.js";
-import { teamList, reservList } from "./script.js";
+import { pokemonList, searchScreen } from "./startScreen.js";
+import {
+  teamList,
+  reservList,
+  deletePokemon,
+  deletePokemonReserv,
+} from "./script.js";
 const teamScreen = document.querySelector(".team-screen");
 const teamButton = document.querySelector("#team-button");
 const backbutton = document.querySelector("#back");
@@ -9,36 +14,58 @@ const pokemonReserveContainer = document.querySelector("#r-team");
 teamButton.addEventListener("click", () => {
   searchScreen.classList.remove("show");
   teamScreen.classList.add("show");
-  console.log("click");
   pokemonTeamContainer.innerText = "";
   teamList.forEach((pokemon) => {
     const element = document.createElement("li");
+    const button = document.createElement("button");
+    button.classList.add("kick-button");
+    element.classList.add("element");
     if (pokemon.bild) {
       const bild = document.createElement("img");
       bild.src = `${pokemon.bild}`;
       element.innerText = `${pokemon.name}`.toUpperCase();
+      button.innerText = "kicka";
       element.appendChild(bild);
+      element.appendChild(button);
       bild.classList.add("bild");
     }
     pokemonTeamContainer.appendChild(element);
+    button.addEventListener("click", (event) => {
+      reservList.push(teamList[0]);
+      deletePokemon(event);
+      teamList.push(reservList[0]);
+      console.log(teamList);
+      console.log(reservList);
+      deletePokemonReserv(event);
+      pokemonTeamContainer.removeChild(element);
+    });
   });
   pokemonReserveContainer.innerText = "";
   reservList.forEach((pokemon) => {
     const element = document.createElement("li");
+    const button = document.createElement("button");
+    element.classList.add("element");
+    button.classList.add("kick-button");
     if (pokemon.bild) {
       const bild = document.createElement("img");
       bild.src = `${pokemon.bild}`;
       element.innerText = `${pokemon.name}`.toUpperCase();
+      button.innerText = "kicka";
       element.appendChild(bild);
+      element.appendChild(button);
       bild.classList.add("bild");
     }
     pokemonReserveContainer.appendChild(element);
+    button.addEventListener("click", (event) => {
+      console.log("click");
+      deletePokemonReserv(event);
+      pokemonReserveContainer.removeChild(element);
+    });
   });
 });
-export { teamButton };
+export { teamButton, pokemonTeamContainer };
 
 backbutton.addEventListener("click", () => {
-  console.log("click");
   searchScreen.classList.add("show");
   teamScreen.classList.remove("show");
 });
